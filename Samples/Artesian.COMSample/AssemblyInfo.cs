24f71afe840983766c11dd0f235078511d1615b6
Marco Giannotti <marco.giannnotti@ark-energy.eu>
2019-11-13 11:56:27 +0100

    Added COMSample for COM Interop integration

---
+++ Samples/Artesian.COMSample/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+[assembly: System.Runtime.InteropServices.Guid("34eebd6a-d8db-46da-980d-3f350f22773d")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
---
+++ Samples/Artesian.COMSample/Ex.cs
@@ -0,0 +1,30 @@
+﻿using Artesian.SDK.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artesian.COMSample
+{
+	public static class Ex
+	{
+		public static ActualTimeSerieRow ConvertToOutput(this TimeSerieRow.Actual input)
+		{
+			var res = new ActualTimeSerieRow()
+			{
+				CompetenceEnd = input.CompetenceEnd.ToString(),
+				CompetenceStart = input.CompetenceStart.ToString(),
+				CurveName = input.CurveName,
+				ProviderName = input.ProviderName,
+				Time = input.Time.ToString(),
+				TSID = input.TSID,
+				Value = input.Value ?? 0
+			};
+
+			return res;
+		}
+
+
+	}
+}
